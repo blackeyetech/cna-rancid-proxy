c8dd080f16438ac0aac8bdf77fb418384fb91c74
Kieran Northime <kieran@blackeyetech.com>
2021-10-18 19:37:15 +0100

    Initial commit

---
+++ bin/setup.sh
@@ -0,0 +1,7 @@
+export CNA_USE_HTTPS=N
+export CNA_HTTP_INTERFACE="wlp7s0"
+export CNA_HTTP_PORT=8800
+export CNA_LOG_LEVEL=DEBUG
+
+export CNA_RANCID_CFG_DIR="/tmp"
+export CNA_RANCID_FQDN="com"
---
+++ src/main.ts
@@ -0,0 +1,83 @@
+// Imports here
+import CNShell, { HttpError } from "cn-shell";
+
+import fs from "fs";
+
+// Rancid proxy config consts here
+const CFG_RANCID_CFG_DIR = "RANCID_CFG_DIR";
+const CFG_RANCID_FQDN = "RANCID_FQDN";
+
+// Route constants here
+const ROUTE_BASE = "/api";
+
+const ROUTE_SWITCH = `${ROUTE_BASE}/switch`;
+
+// HTTP Prop Patterns here
+
+process.on("unhandledRejection", error => {
+  // Will print "unhandledRejection err is not defined"
+  console.log("unhandledRejection", error);
+});
+// CNRancidProxy class here
+class CNRancidProxy extends CNShell {
+  // Properties here
+  private _cfgDir: string;
+  private _fqdn: string;
+
+  // Constructor here
+  constructor(name: string) {
+    super(name);
+
+    this._cfgDir = this.getRequiredCfg(CFG_RANCID_CFG_DIR);
+    this._fqdn = this.getCfg(CFG_RANCID_FQDN, "");
+
+    if (this._fqdn.length) {
+      this._fqdn = `.${this._fqdn}`;
+    }
+
+    this.setupRoutes();
+  }
+
+  // Abstract method implementations here
+  async start(): Promise<boolean> {
+    return true;
+  }
+
+  async stop(): Promise<void> {
+    return;
+  }
+
+  async healthCheck(): Promise<boolean> {
+    return true;
+  }
+
+  // Public methods here
+  private setupRoutes() {
+    this.simpleReadRoute(ROUTE_SWITCH, async id => this.get(id), true, false);
+  }
+
+  private async get(switchName: string): Promise<string> {
+    let fileName = `${this._cfgDir}/${switchName}${this._fqdn}`;
+    let config = "";
+
+    this.debug("Searching for switch config", fileName);
+
+    try {
+      config = fs.readFileSync(fileName, "utf-8");
+    } catch (e) {
+      this.error(e);
+
+      let error: HttpError = {
+        status: 404,
+        message: `Can not find config file ${fileName}`,
+      };
+
+      throw error;
+    }
+
+    return config;
+  }
+}
+
+let proxy = new CNRancidProxy("RancidProxy");
+proxy.init();
